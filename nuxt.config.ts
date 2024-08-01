38d95ae4d8785f2a6e1f14e0ecfcf6235b90f7b4
Anggairham <anggairham08@gmail.com>
2024-08-01 22:40:30 +0700

    update config baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   app: {
+    baseURL: "/nuxt-crud",
+    buildAssetsDir: "/_nuxt/",
     head: {
       title:"NUXT 3 CRUD",
       meta:[
